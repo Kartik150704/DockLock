8ef80e5966f058ae110b39f9e3dba32d8b03f651
Kartik150704 <108309065+Kartik150704@users.noreply.github.com>
2023-10-06 03:31:55 +0530

    change

---
+++ Backend/Components/server runner/ServerRunner.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const crypto = require('crypto')
+const fs = require('fs')
+const axios = require('axios');
+const database = require('../Database/Database.js');
+
+const router = express.Router()
+
+router.get("/runserver", async (req, resp) => {
+
+    
+    axios.get('https://serverrunner.onrender.com/fakeserver')
+    .then((response) => {
+        
+    })
+    .catch((error) => {
+       
+    });
+    resp.send({ server: true })
+})
+
+
+
+
+
+module.exports = router
